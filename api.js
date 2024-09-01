5b1737a76e1ad879f9b777f50be1c61bc6ed9e5b
Vahid <150700164+Vahidsk18@users.noreply.github.com>
2024-09-01 21:05:30 +0530

    commit

---
+++ api.js
@@ -0,0 +1,27 @@
+// let API="https://dog.ceo/api/breeds/image/random";
+
+// async function api(){
+//     let data=await fetch(API)
+//     let response=await data.json()
+//     store=response.message
+//     // console.log(store);
+//     display(store)
+
+// }
+
+// api()
+
+// // display image 
+// let body=document.querySelector("body")
+
+// function display(store){
+//     image=document.createElement("img")
+//     image.src=store
+//     body.appendChild(image)
+
+// }
+
+
+let arr=[1,2,3,4,5]
+let m=arr.forEach(arr=> arr*2)
+console.log(m);
